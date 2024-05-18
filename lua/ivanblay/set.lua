9f830f9d0304b597b6fd8762cc94645dbf1110d2
ivabarlay <ivabarlay@alum.us.es>
2024-05-18 13:03:29 +0200

    New map for netrw and column color removed

--- lua/ivanblay/set.lua
+++ lua/ivanblay/set.lua
@@ -1,7 +1,6 @@
 vim.opt.relativenumber = true
 vim.opt.smartindent = true
 vim.opt.termguicolors = true
-vim.opt.colorcolumn = "80"
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
@@ -13,5 +12,4 @@ vim.opt.hlsearch = false
 vim.opt.incsearch = true
 
 vim.opt.scrolloff = 8
-vim.opt.colorcolumn = "80"
 vim.g.mapLeader = " "
