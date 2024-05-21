a7682b7f26c3a2569a0a03b5af77095f85f7f40c
ivabarlay <ivabarlay@alum.us.es>
2024-05-21 13:47:13 +0200

    confirm cambiado de ctrl+y a ctrl+l

--- lua/ivanblay/set.lua
+++ lua/ivanblay/set.lua
@@ -10,11 +10,5 @@ vim.opt.wrap = false
 
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
-
-vim.opt.swapfile = false
-vim.opt.backup = false
-vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
-vim.opt.undofile = true
-
 vim.opt.scrolloff = 8
 vim.g.mapLeader = " "
