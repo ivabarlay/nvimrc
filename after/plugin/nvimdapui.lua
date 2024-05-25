3ca322fb204cecb5ecd57489ee7f4f060018f937
ivabarlay <ivabarlay@alum.us.es>
2024-05-25 13:01:04 +0200

    modified:   after/plugin/nvimdapui.lua

--- after/plugin/nvimdapui.lua
+++ after/plugin/nvimdapui.lua
@@ -5,10 +5,4 @@ end
 dap.listeners.before.launch.dapui_config = function()
   dapui.open()
 end
-dap.listeners.before.event_terminated.dapui_config = function()
-  dapui.close()
-end
-dap.listeners.before.event_exited.dapui_config = function()
-  dapui.close()
-end
 require("dapui").setup()
