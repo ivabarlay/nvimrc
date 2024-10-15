8b8f4a47fa31f7b35c7396f40d494bd8132a66a0
ivabarlay <ivabarlay@alum.us.es>
2024-10-15 23:31:12 +0200

    whichkey fixed

--- lua/plugins/whichkey.lua
+++ lua/plugins/whichkey.lua
@@ -9,17 +9,27 @@ return {
         which_key.setup()
 
         -- Register prefixes for the different key mappings we have setup previously
-        which_key.register({
-            ['<leader>/'] = {name = "Comments", _ = 'which_key_ignore'},
-            ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-            ['<leader>d'] = {name = '[D]ebug' , _ = 'which_key_ignore' },
-            ['<leader>e'] = {name = '[E]xplorer', _ = 'which_key_ignore'},
-            ['<leader>f'] = { name = '[F]ind', _ = 'which_key_ignore' },
-            ['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },
-            ['<leader>J'] = { name = '[J]ava', _ = 'which_key_ignore' },
-            ['<leader>t'] = { name = '[T]ab', _ = 'which_key_ignore'},
-            ['<leader>w'] = {name = '[W]indow', _ = 'which_key_ignore'},
-            ['<leader>u'] = {name = '[U]ndo', _ = 'which_key_ignore'}
+        which_key.add({
+            { "<leader>/", group = "Comments" },
+            { "<leader>/_", hidden = true },
+            { "<leader>J", group = "[J]ava" },
+            { "<leader>J_", hidden = true },
+            { "<leader>c", group = "[C]ode" },
+            { "<leader>c_", hidden = true },
+            { "<leader>d", group = "[D]ebug" },
+            { "<leader>d_", hidden = true },
+            { "<leader>e", group = "[E]xplorer" },
+            { "<leader>e_", hidden = true },
+            { "<leader>f", group = "[F]ind" },
+            { "<leader>f_", hidden = true },
+            { "<leader>g", group = "[G]it" },
+            { "<leader>g_", hidden = true },
+            { "<leader>t", group = "[T]ab" },
+            { "<leader>t_", hidden = true },
+            { "<leader>u", group = "[U]ndo" },
+            { "<leader>u_", hidden = true },
+            { "<leader>w", group = "[W]indow" },
+            { "<leader>w_", hidden = true },
         })
     end
 }
